2158d8c349b40bd14db90ecf6dfe1d1a43f1b04a
et1911 <et1911@et.com>
2020-05-19 15:01:42 +0800

    第一次提交

---
+++ springmvc-003/src/main/java/com/etoak/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.etoak.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/hello")
+public class HelloController {
+	@RequestMapping("/index")
+	public String index(HttpServletRequest request) {
+		//获取参数
+		String name = request.getParameter("name");
+		System.out.println("param name - " + name);
+		//向request域传值
+		request.setAttribute("result", "Hello " + name);
+		return "hello/index";
+	}
+}
